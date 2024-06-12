cf784cf8f259ab9c51a0d3bbeda03e8a11010838
Gillan Flaherty <flahertygillian@gmail.com>
2024-06-12 13:46:50 -0500

    FE complete

--- web/src/pages/viewAnimals.js
+++ web/src/pages/viewAnimals.js
@@ -9,7 +9,7 @@ import DataStore from "../util/DataStore";
 class ViewAnimals extends BindingClass {
     constructor() {
         super();
-        this.bindClassMethods(['clientLoaded', 'mount', 'addAnimalsToPage', 'addAnimal', 'removeAnimal'], this);
+        this.bindClassMethods(['clientLoaded', 'mount', 'addAnimalsToPage'], this);
         this.dataStore = new DataStore();
         this.dataStore.addChangeListener(this.addAnimalsToPage);
         this.header = new Header(this.dataStore);
@@ -25,18 +25,16 @@ class ViewAnimals extends BindingClass {
         document.getElementById('habitat-name').innerText = "Loading Habitat ...";
         const habitat = await this.client.getHabitat(habitatId);
         this.dataStore.set('habitat', habitat);
-        document.getElementById('animals').innerText = "(loading animals...)"
-        const animals = await this.client.getAnimalsForHabitat(habitatId);
-        this.dataStore.set('animals', animals);
+        document.getElementById('animals').innerText = "(loading animals...)";
+        const habitatAnimals = await this.client.getAnimalsForHabitat(habitatId);
+        console.log(habitatAnimals);
+        this.dataStore.set('habitat-animals', habitatAnimals);
     }
 
     /**
      * Add the header to the page and load the AnimalEnrichmentTrackerClient.
      */
     mount() {
-        document.getElementById('add-animal-btn').addEventListener('click', this.addAnimal);
-        document.getElementById('animals').addEventListener('click', this.removeAnimal);
-
         this.header.addHeaderToPage();
 
         this.client = new AnimalEnrichmentTrackerClient();
@@ -50,9 +48,9 @@ class ViewAnimals extends BindingClass {
      * When the list of animals are updated in the datastore, update the animals metadata on the page.
      */
     addAnimalsToPage() {
-        const animals = this.dataStore.get('animals');
+        const habitatAnimals = this.dataStore.get('habitat-animals');
         const habitat = this.dataStore.get('habitat');
-        if (animals == null || habitat == null) {
+        if (habitatAnimals == null || habitat == null) {
             return;
         }
 
@@ -68,84 +66,21 @@ class ViewAnimals extends BindingClass {
         }
         document.getElementById('species').innerHTML = speciesHtml;
 
-        let animalsHtml = '<table id="animals-table"><tr><th>Animal Name</th><th>Remove</th></tr>';
+        let animalsHtml = '<table id="animals-table"><tr><th>ID</th><th>Name</th><th>Age</th><th>Sex</th><th>Species</th></tr>';
         let animal;
-        for (animal of animals) {
+        for (animal of habitatAnimals) {
             animalsHtml += `
-               <tr id="animal-${animal}">
-                   <td>${animal}</td>
-                   <td><button data-animal-name="${animal}" class="button remove-animal">Remove</button></td>
+               <tr id="${animal.animalId}">
+                   <td>${animal.animalId}</td>
+                   <td>${animal.animalName}</td>
+                   <td>${animal.age}</td>
+                   <td>${animal.sex}</td>
+                   <td>${animal.species}</td>
                </tr>`;
         }
 
         animalsHtml += '</table>';
         document.getElementById('animals').innerHTML = animalsHtml;
-    }
-
-
-    /*
-    * method to run when the add animal submit button is pressed. Call the AnimalEnrichmentTrackerClient to add
-    * an animal to a habitat's list of animals.
-    */
-    async addAnimal() {
-        const errorMessageDisplay = document.getElementById('add-animal-error-message');
-        errorMessageDisplay.innerText = ``;
-        errorMessageDisplay.classList.add('hidden');
-
-        const habitat = this.dataStore.get('habitat');
-        if (habitat == null) {
-            return;
-        }
-
-        document.getElementById('add-animal-btn').innerText = 'Adding...';
-        const animalToBeAdded = document.getElementById('animal-name').value;
-        const habitatId = habitat.habitatId;
-
-        const animals = await this.client.addAnimalToHabitat(habitatId, animalToBeAdded, (error) => {
-           errorMessageDisplay.innerText = `Error: ${error.message}`;
-           errorMessageDisplay.classList.remove('hidden');
-           this.showErrorModal(error.message);
-        });
-
-        this.dataStore.set('animals', animals);
-        document.getElementById('add-animal-btn').innerText = 'Add';
-
-        if (animals.includes(animalToBeAdded)) {
-            location.reload();
-        }
-    }
-
-    /*
-    * method to run when the remove animal button is pressed. Call the AnimalEnrichmentTrackerClient to remove
-    * an animal from a habitat's list of animals.
-    */
-    async removeAnimal(e) {
-        const errorMessageDisplay = document.getElementById('error-message');
-        errorMessageDisplay.innerText = ``;
-        errorMessageDisplay.classList.add('hidden');
-
-        const habitat = this.dataStore.get('habitat');
-        if (habitat == null) {
-            return;
-        }
-
-        const removeButton = e.target;
-        if (!removeButton.classList.contains("remove-animal")) {
-            return;
-        }
-
-        removeButton.innerText = "Removing...";
-        const habitatId = habitat.habitatId;
-
-        const animalName = removeButton.dataset.animalName;
-        const animals = await this.client.removeAnimalFromHabitat(habitatId, animalName, (error) => {
-           errorMessageDisplay.innerText = `Error: ${error.message}`;
-           errorMessageDisplay.classList.remove('hidden');
-           this.showErrorModal(error.message);
-        });
-
-        this.dataStore.set('animals', animals);
-        removeButton.innerText = "Remove";
     }
 
     async showErrorModal(message) {
