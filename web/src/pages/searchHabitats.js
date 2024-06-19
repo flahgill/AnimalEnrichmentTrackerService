f43a197854200c90cd6767cccc3c2e0a4c05ccd7
Gillan Flaherty <flahertygillian@gmail.com>
2024-06-19 16:40:58 -0500

    fixed search boxes to be accordian style for a design

--- web/src/pages/searchHabitats.js
+++ web/src/pages/searchHabitats.js
@@ -3,17 +3,6 @@ import Header from '../components/header';
 import BindingClass from "../util/bindingClass";
 import DataStore from "../util/DataStore";
 
-/*
-The code below this comment is equivalent to...
-const EMPTY_DATASTORE_STATE = {
-    'search-criteria': '',
-    'search-results': [],
-};
-
-...but uses the "KEY" constants instead of "magic strings".
-The "KEY" constants will be reused a few times below.
-*/
-
 const SEARCH_CRITERIA_KEY = 'search-criteria';
 const SEARCH_RESULTS_KEY = 'search-results';
 const EMPTY_DATASTORE_STATE = {
@@ -21,10 +10,6 @@ const EMPTY_DATASTORE_STATE = {
     [SEARCH_RESULTS_KEY]: [],
 };
 
-
-/**
- * Logic needed for the search habitats page of the website.
- */
 export default class SearchHabitats extends BindingClass {
     constructor(client) {
         super();
@@ -32,37 +17,25 @@ export default class SearchHabitats extends BindingClass {
 
         this.bindClassMethods(['mount', 'search', 'displaySearchResults', 'getHTMLForSearchResults'], this);
 
-        // Create a enw datastore with an initial "empty" state.
         this.dataStore = new DataStore(EMPTY_DATASTORE_STATE);
         this.header = new Header(this.dataStore);
         this.dataStore.addChangeListener(this.displaySearchResults);
         console.log("searchHabitats constructor");
     }
 
-    /**
-     * Add the header to the page and load the AnimalEnrichmentTrackerClient.
-     */
     mount() {
-        // Wire up the form's 'submit' event and the button's 'click' event to the search method.
         document.getElementById('search-habitats-form').addEventListener('submit', this.search);
         document.getElementById('search-btn').addEventListener('click', this.search);
 
         this.header.addHeaderToPage();
     }
 
-    /**
-     * Uses the client to perform the search,
-     * then updates the datastore with the criteria and results.
-     * @param evt The "event" object representing the user-initiated event that triggered this method.
-     */
     async search(evt) {
-        // Prevent submitting the from from reloading the page.
         evt.preventDefault();
 
         const searchCriteria = document.getElementById('search-criteria').value;
         const previousSearchCriteria = this.dataStore.get(SEARCH_CRITERIA_KEY);
 
-        // If the user didn't change the search criteria, do nothing
         if (previousSearchCriteria === searchCriteria) {
             return;
         }
@@ -83,9 +56,6 @@ export default class SearchHabitats extends BindingClass {
         document.getElementById('search-btn').innerText = "Search";
     }
 
-    /**
-     * Pulls search results from the datastore and displays them on the html page.
-     */
     displaySearchResults() {
         const searchCriteria = this.dataStore.get(SEARCH_CRITERIA_KEY);
         const searchResults = this.dataStore.get(SEARCH_RESULTS_KEY);
@@ -105,11 +75,6 @@ export default class SearchHabitats extends BindingClass {
         }
     }
 
-    /**
-     * Create appropriate HTML for displaying searchResults on the page.
-     * @param searchResults An array of habitats objects to be displayed on the page.
-     * @returns A string of HTML suitable for being dropped on the page.
-     */
     getHTMLForSearchResults(searchResults) {
         if (searchResults.length === 0) {
             return '<h4>No results found</h4>';
@@ -134,8 +99,5 @@ export default class SearchHabitats extends BindingClass {
         html += '</table>';
 
         return html;
-
-        return html;
     }
-
 }
